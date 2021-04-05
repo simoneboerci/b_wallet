ded90d191fa0f977399e05abc55eda228d17adc1
Simone <boercisimone@gmail.com>
2021-04-05 13:52:06 +0200

    OTA updates

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -4,6 +4,7 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 
 /**
  * Generated file. Do not edit.
@@ -13,9 +14,12 @@ import io.flutter.embedding.engine.FlutterEngine;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
+    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.firestore.FlutterFirebaseFirestorePlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.auth.FlutterFirebaseAuthPlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.core.FlutterFirebaseCorePlugin());
+    flutterEngine.getPlugins().add(new io.flutter.plugins.firebase.storage.FlutterFirebaseStoragePlugin());
     flutterEngine.getPlugins().add(new io.flutter.plugins.googlesignin.GoogleSignInPlugin());
+      sk.fourq.otaupdate.OtaUpdatePlugin.registerWith(shimPluginRegistry.registrarFor("sk.fourq.otaupdate.OtaUpdatePlugin"));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:ota_update/ota_update.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import './pages/home_page.dart';
 
 Future main() async{
@@ -12,19 +13,81 @@ Future main() async{
   runApp(BWallet());
 }
 
-class BWallet extends StatelessWidget {
+class BWallet extends StatefulWidget {
+
+  final String appVersion = "0.0.1";
+  final bool releaseVersion = false;
 
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  @override
+  _BWalletState createState() => _BWalletState();
+}
+
+class _BWalletState extends State<BWallet> {
+ OtaEvent currentEvent;
+
+ @override
+  void initState() {
+    super.initState();
+    checkForUpdates();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: auth.currentUser == null ? LoginPage(firebaseFirestore) : HomePage(firebaseFirestore)
-    );
+   return MaterialApp(
+       title: 'BWallet',
+       theme: ThemeData(
+         primarySwatch: Colors.blue,
+       ),
+       home: widget.auth.currentUser == null ? LoginPage(widget.firebaseFirestore) : HomePage(widget.firebaseFirestore)
+   );
+  }
+
+  Future<void> checkForUpdates() async{
+   if(await needToUpdate())
+     await tryOtaUpdate();
+  }
+
+  Future<void> tryOtaUpdate() async{
+
+   String apkUrl = await FirebaseStorage.instance.ref().child("BWallet.apk").getDownloadURL();
+
+    try{
+      OtaUpdate().execute(
+        apkUrl,
+        destinationFilename: "BWallet.apk",
+      ).listen((event) {
+        setState(() => currentEvent = event);
+      });
+    }catch(e){
+
+    }
+  }
+
+  Future<bool> needToUpdate() async{
+   String latestVersion;
+
+   DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection("app").doc("versions").get();
+
+   if(widget.releaseVersion)
+     latestVersion = snapshot.data()["release"];
+   else
+     latestVersion = snapshot.data()["debug"];
+
+   if(int.parse(widget.appVersion.split(".")[0]) < int.parse(latestVersion.split(".")[0]))
+     return true;
+   else{
+     if(int.parse(widget.appVersion.split(".")[1]) < int.parse(latestVersion.split(".")[1]))
+       return true;
+     else{
+       if(int.parse(widget.appVersion.split(".")[2]) < int.parse(latestVersion.split(".")[2]))
+         return true;
+     }
+   }
+
+   return false;
   }
 }
